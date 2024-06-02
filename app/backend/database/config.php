227cc2f6d683001ae0345640a13ef00b4beb9b2f
NatoRodrigues <renwise2.0@gmail.com>
2024-06-02 00:05:10 -0300

    Configuração do backend

--- app/backend/database/config.php
+++ app/backend/database/config.php
@@ -1,19 +1,24 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = NULL;
-$database = "amg8833";
+// Obtenha a URL do JawsDB a partir das variáveis de ambiente
+$url = parse_url(getenv('JAWSDB_URL'));
 
-$conn = new mysqli($servername, $username, $password, $database);
+$servername = $url['sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username = $url['ni26ybmxp72pcyeh'];
+$password = $url['r4lrxmphrn44ftyr'];
+$database = substr($url['h138o4z12q7moerr'], 1);
+$port = $url['3306'];
 
+// Crie a conexão
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Verifique a conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
-else {
-    
-}
 
+// Defina o charset
 $conn->set_charset("utf8");
 
+// Se a conexão for bem-sucedida, você pode prosseguir com suas operações
 ?>
